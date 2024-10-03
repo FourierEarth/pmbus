3f3c270e8ba425b535da450c52653fc75f69d7cf
Jacob Birkett <jacob_birkett@fourier.earth>
2024-10-03 04:17:43 -0700

    macros: impl_commands: generate all constants

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,19 +1,31 @@
 use proc_macro::TokenStream as TokenStream1;
-use quote::quote;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::{quote, ToTokens};
 use syn::parse_macro_input;
 
-use self::pmbus::CommandsTable;
+use self::pmbus::{CommandIdent, CommandsTable};
 
 #[proc_macro]
 pub fn impl_commands(input: TokenStream1) -> TokenStream1 {
     let table: CommandsTable = parse_macro_input!(input);
-    dbg!(table);
-    quote! {
-        #[derive(Copy, Clone, PartialEq, Eq, Debug)]
-        pub enum Command {
+
+    let mut constants = TokenStream2::new();
+
+    for entry in table.0.iter() {
+        match &entry.ident {
+            CommandIdent::Undefined => continue,
+            CommandIdent::Verbatim(ident) => {
+                // Maybe it would have been better to just keep the `LitInt` around, because now we have to format it as hexadecimal again.
+                let byte = entry.byte.1;
+                quote! {
+                    pub const #ident: u8 = #byte;
+                }
+                .to_tokens(&mut constants);
+            }
         }
     }
-    .into()
+
+    constants.into()
 }
 
 mod pmbus {
@@ -38,7 +50,7 @@ mod pmbus {
         // UNDEFINED / RESERVED∏
         Undefined,
         // CONSTANT IDENTIFIER
-        Verbatim(String),
+        Verbatim(Ident),
     }
 
     impl Parse for CommandIdent {
@@ -51,9 +63,7 @@ mod pmbus {
                 input.parse::<Token![_]>().unwrap();
                 Ok(Self::Undefined)
             } else {
-                input
-                    .parse::<Ident>()
-                    .map(|ident| Self::Verbatim(ident.to_string()))
+                input.parse().map(Self::Verbatim)
             }
         }
     }
