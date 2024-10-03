a779de5e94d9d1932501923fe6e563bb697977fc
Jacob Birkett <jacob_birkett@fourier.earth>
2024-10-02 23:21:40 -0700

    smbus: doc: add page numbers after sections

--- src/smbus.rs
+++ src/smbus.rs
@@ -22,7 +22,7 @@ pub const SMBUS_MAX_BLOCK_SIZE: usize = 32;
 ///
 /// The implementation is not comprehensive, and omits several legacy transactions including read/write N-bytes protocols.
 ///
-/// Also missing is SMBus Host Notify protocol (6.5.9) which will later be necessary
+/// Also missing is SMBus Host Notify protocol (6.5.9, Pg. 44) which will later be necessary
 /// to interrupt the host when a device on the bus has set the `SMBALERT#` bit.
 /// This is not critical, as there are often alternative ways to listen for that signal.
 ///
@@ -30,7 +30,7 @@ pub const SMBUS_MAX_BLOCK_SIZE: usize = 32;
 /// PEC can be implemented later with either a `const` boolean generic parameter,
 /// or with a Cargo feature.
 pub trait SmBus<A: AddressMode = SevenBitAddress>: I2c<A> {
-    /// 6.5.1
+    /// 6.5.1, Pg. 38
     async fn quick_command(&mut self, address: A, bit: bool) -> Result<(), Self::Error> {
         if bit {
             self.read(address, &mut []).await
@@ -39,44 +39,44 @@ pub trait SmBus<A: AddressMode = SevenBitAddress>: I2c<A> {
         }
     }
 
-    /// 6.5.2
+    /// 6.5.2, Pg. 38-39
     async fn send_byte(&mut self, address: A, byte: u8) -> Result<(), Self::Error> {
         self.write(address, &[byte]).await
     }
 
-    /// 6.5.3
+    /// 6.5.3, Pg. 39
     async fn receive_byte(&mut self, address: A) -> Result<u8, Self::Error> {
         let mut buf = [0x00];
         self.read(address, &mut buf).await?;
         Ok(buf[0])
     }
 
-    /// 6.5.4
+    /// 6.5.4, Pg. 39-40
     async fn write_byte(&mut self, address: A, command: u8, byte: u8) -> Result<(), Self::Error> {
         self.write(address, &[command, byte]).await
     }
 
-    /// 6.5.4
+    /// 6.5.4, Pg. 39-40
     async fn write_word(&mut self, address: A, command: u8, word: u16) -> Result<(), Self::Error> {
         let word = word.to_le_bytes();
         self.write(address, &[command, word[0], word[1]]).await
     }
 
-    /// 6.5.5
+    /// 6.5.5, Pg. 40-41
     async fn read_byte(&mut self, address: A, command: u8) -> Result<u8, Self::Error> {
         let mut buf = [0x00];
         self.write_read(address, &[command], &mut buf).await?;
         Ok(buf[0])
     }
 
-    /// 6.5.5
+    /// 6.5.5, Pg. 40-41
     async fn read_word(&mut self, address: A, command: u8) -> Result<u16, Self::Error> {
         let mut buf = [0x00, 0x00];
         self.write_read(address, &[command], &mut buf).await?;
         Ok(u16::from_le_bytes(buf))
     }
 
-    /// 6.5.6
+    /// 6.5.6, Pg. 41
     async fn process_call(&mut self, address: A, command: u8, word: u16) -> Result<u16, Self::Error> {
         let word = word.to_le_bytes();
         let mut buf = [0x00, 0x00];
@@ -84,13 +84,13 @@ pub trait SmBus<A: AddressMode = SevenBitAddress>: I2c<A> {
         Ok(u16::from_le_bytes(buf))
     }
 
-    /// 6.5.7
+    /// 6.5.7, Pg. 42
     async fn block_write(&mut self, address: A, command: u8, block: &[u8]) -> Result<(), Self::Error> {
         assert!(block.len() <= SMBUS_MAX_BLOCK_SIZE);
         self.transaction(address, &mut [Operation::Write(&[command, block.len() as u8]), Operation::Write(block)]).await
     }
 
-    /// 6.5.7
+    /// 6.5.7, Pg. 42
     async fn block_read(&mut self, address: A, command: u8) -> Result<Vec<u8>, Self::Error> {
         // The first byte is reserved for the size of the data written back.
         // Later, when PEC is implemented, we will need an extra byte at the end also.
@@ -103,7 +103,7 @@ pub trait SmBus<A: AddressMode = SevenBitAddress>: I2c<A> {
         Ok(block.to_vec())
     }
 
-    /// 6.5.8
+    /// 6.5.8, Pg. 43-44
     async fn block_process_call(&mut self, address: A, command: u8, write_block: &[u8]) -> Result<Vec<u8>, Self::Error> {
         assert!(write_block.len() <= SMBUS_MAX_BLOCK_SIZE);
         let mut buf = Vec::with_capacity(SMBUS_MAX_BLOCK_SIZE + 1);
